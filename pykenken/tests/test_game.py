4a438872f394d1344701673c1e145e351312fe05
Daniel Zawada <daniel.zawada@usask.ca>
2018-03-24 10:56:08 -0600

    Fix broken test

--- pykenken/tests/test_game.py
+++ pykenken/tests/test_game.py
@@ -6,8 +6,8 @@ def test_generate_board_unique():
     size = 6
     kenken = KenKen.generate(size)
 
-    for row in kenken.board:
+    for row in kenken.board.reshape((size, size)):
         np.testing.assert_equal(np.sort(row), np.unique(row))
 
-    for col in kenken.board.T:
+    for col in kenken.board.reshape((size, size)).T:
         np.testing.assert_equal(np.sort(col), np.unique(col))
